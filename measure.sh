700f350ef51d9527d4a6c2fac8807bafda7cfbe4
Justus Stahlhut <justus.stahlhut@t-online.de>
2025-01-16 21:05:49 +0100

    add run
    Signed-off-by: Justus Stahlhut <justus.stahlhut@t-online.de>

--- measure.sh
+++ measure.sh
@@ -9,6 +9,7 @@ for i in {1..10}; do
 done
 
 TIMES=$(awk '{ print $(NF-1) }' "${RUN_FILE}")
+# DOES NOT WORK IF TIME < 1min
 AVG_TIME=$(echo "${TIMES}" | awk -F: '{ sum += $1*60 + $2 } END { print sum / 10 }')
 TIME_PER_EPOCH=$( echo "scale=2; $AVG_TIME / $EPOCHS" | bc -l)
 
